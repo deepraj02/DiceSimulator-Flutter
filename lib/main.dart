0ec24eddbcda1f15d6c7109ea9b8249adc302d45
Deepraj Baidya <63138398+deepraj02@users.noreply.github.com>
2022-02-20 22:41:44 +0530

    First Release

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Color.fromARGB(255, 97, 4, 4),
+          appBar: AppBar(
+            title: Center(child: Text("Dice Simulator App")),
+            backgroundColor: Color.fromARGB(255, 175, 8, 8),
+          ),
+          body: diceSimulator(),
+        ),
+      ),
+    );
+
+class diceSimulator extends StatefulWidget {
+  const diceSimulator({Key? key}) : super(key: key);
+
+  @override
+  _diceSimulatorState createState() => _diceSimulatorState();
+}
+
+class _diceSimulatorState extends State<diceSimulator> {
+  int mainDice = 1;
+  void diceFaceChanger() {
+    setState(() {
+      mainDice = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            //Used to resize elements acc to screen
+            child: TextButton(
+              onPressed: () {
+                diceFaceChanger();
+              },
+              child: Image(
+                image: AssetImage("images/dice$mainDice.png"),
+              ),
+            ),
+          ),
+          /* Expanded(
+            //Used to resize elements acc to screen
+            child: TextButton(
+              onPressed: () => print("Button 2 Pressed"),
+              child: Image.asset("images/dice1.png"),
+            ),
+          ), */
+        ],
+      ),
+    );
+  }
+}
