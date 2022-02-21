aa414286747580d442136b82dcfa625d3ae62a41
Deepraj Baidya <63138398+deepraj02@users.noreply.github.com>
2022-02-21 21:26:00 +0530

    Changes in UI

--- lib/main.dart
+++ lib/main.dart
@@ -32,19 +32,35 @@ class _diceSimulatorState extends State<diceSimulator> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Row(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: <Widget>[
           Expanded(
-            //Used to resize elements acc to screen
-            child: TextButton(
-              onPressed: () {
-                diceFaceChanger();
-              },
-              child: Image(
-                image: AssetImage("images/dice$mainDice.png"),
+            child: Center(
+              child: Text(
+                "Click Dice",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 26,
+                ),
               ),
             ),
           ),
+          Container(
+            height: 600,
+            child: Expanded(
+              //Used to resize elements acc to screen
+              child: FlatButton(
+                onPressed: () {
+                  diceFaceChanger();
+                },
+                child: Image(
+                  image: AssetImage("images/dice$mainDice.png"),
+                ),
+              ),
+            ),
+          ),
+
           /* Expanded(
             //Used to resize elements acc to screen
             child: TextButton(
